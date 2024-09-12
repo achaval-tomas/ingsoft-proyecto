4a8d688469d840fea62e3c3a68aaf7e097952a60
Tomás Peyronel <tpeyronel@gmail.com>
2024-09-12 18:07:37 -0300

    reformat all

--- eslint.config.js
+++ eslint.config.js
@@ -1,49 +1,52 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
-    files: ['**/*.{ts,tsx}'],
-    settings: {
-      react: { version: '18.3' },
-    },
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      indent: ["warn", 4, { SwitchCase: 1 }],
-      quotes: ["warn", "double"],
-      semi: ["error", "always"],
-      "comma-dangle": ["warn", "always-multiline"],
-      "comma-spacing": ["warn", { before: false, after: true }],
-      eqeqeq: ["error", "always", { null: "ignore" }],
-      "no-trailing-spaces": "warn",
-      "object-curly-spacing": ["warn", "always"],
-      "arrow-spacing": ["warn", { before: true, after: true }],
-      "no-multi-spaces": "warn",
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+    { ignores: ["dist"] },
+    {
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
+        ],
+        files: ["**/*.{ts,tsx}"],
+        settings: {
+            react: { version: "18.3" },
+        },
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        plugins: {
+            react,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        rules: {
+            indent: ["warn", 4, { SwitchCase: 1 }],
+            quotes: ["warn", "double"],
+            semi: ["error", "always"],
+            "comma-dangle": ["warn", "always-multiline"],
+            "comma-spacing": ["warn", { before: false, after: true }],
+            eqeqeq: ["error", "always", { null: "ignore" }],
+            "no-trailing-spaces": "warn",
+            "object-curly-spacing": ["warn", "always"],
+            "arrow-spacing": ["warn", { before: true, after: true }],
+            "no-multi-spaces": "warn",
+            ...react.configs.recommended.rules,
+            ...react.configs["jsx-runtime"].rules,
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+        },
+    }
+);
