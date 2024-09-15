554a49afb76f1f24a066a6c5688786caaab76048
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-09-14 23:37:51 -0300

    style

--- src/containers/App/components/MainPageLayout.tsx
+++ src/containers/App/components/MainPageLayout.tsx
@@ -24,28 +24,30 @@ function MainPageLayout({
     }
 
     return (
-        <div>
-        { showCreateForm ? 
-            <div>
-                <CreateLobbyForm 
-                    inputs={lobbyFormInputs}
-                    handleChange={handleLobbyFormChange} 
-                    handleSubmit={handleLobbyFormSubmit} 
-                />
-                <FancyButton onClick={handleGoBack}>
-                    <p>Atrás</p>
-                </FancyButton>
+        <div className="w-screen flex justify-center">
+            <div className="w-7/12 flex justify-around p-8 bg-zinc-700 rounded">
+                { showCreateForm ? 
+                    <>
+                        <CreateLobbyForm 
+                            inputs={lobbyFormInputs}
+                            handleChange={handleLobbyFormChange} 
+                            handleSubmit={handleLobbyFormSubmit} 
+                        />
+                        <FancyButton onClick={handleGoBack}>
+                            <p>Atrás</p>
+                        </FancyButton>
+                    </>
+                    :
+                    <>
+                        <FancyButton onClick={() => setCreateForm(true)}>
+                            <p>Crear lobby</p>
+                        </FancyButton>
+                        <FancyButton onClick={() => {}}>
+                            <p>Unirse a lobby</p>
+                        </FancyButton>
+                    </>
+                }
             </div>
-            :
-            <div>
-                <FancyButton onClick={() => setCreateForm(true)}>
-                    <p>Crear lobby</p>
-                </FancyButton>
-                <FancyButton onClick={() => {}}>
-                    <p>Unirse a lobby</p>
-                </FancyButton>
-            </div>
-        }
         </div>
     );
 }
