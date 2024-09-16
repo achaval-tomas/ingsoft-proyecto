89899283c2ef10f8c3d715f42c368db23431dffb
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-09-16 10:38:55 -0300

    SCRUM-8 PlayerNameForm component
    this component has a single input and receives the value for that input,
    the handler to change the input and the handler to submit the form

---
+++ src/containers/App/components/PlayerNameForm.tsx
@@ -0,0 +1,37 @@
+interface PlayerNameFormProps {
+    name: string,
+    handleChange: (e: React.FormEvent<HTMLInputElement>) => void,
+    handleSubmit: React.FormEventHandler<HTMLFormElement>,
+}
+
+function PlayerNameForm({ name, handleChange, handleSubmit }: PlayerNameFormProps) {
+    const submitDisabled = name === "";
+
+    return (
+        <form 
+            onSubmit={handleSubmit} 
+        >
+            <div className="grid grid-rows-2 grid-cols-2">
+                <label htmlFor="player-name" className="col-span-1 row-span-1">Nombre de jugador:</label>
+                <input
+                    type="text"
+                    name="player-name"
+                    value={name || ""}
+                    onChange={handleChange}
+                    className="col-span-1 row-span-1"
+                >
+                </input>
+                { submitDisabled && <p className="text-sm col-start-2 text-red-400">Elegí un nombre</p>}
+            </div>
+                <button 
+                    className={"fancy-button " + (submitDisabled ? "bg-gray-500" : "")}
+                    type="submit" 
+                    disabled={submitDisabled}
+                >
+                    Crear
+                </button>
+        </form>
+    );
+}
+
+export default PlayerNameForm;
