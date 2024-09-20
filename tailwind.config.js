751789bcf3f76c020b5978600a4e6cc29c78ecee
Tomás Peyronel <tpeyronel@gmail.com>
2024-09-20 20:59:55 -0300

    define common colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: colors.teal,
+                surface: colors.zinc,
+            },
+        },
     },
     plugins: [],
 };
