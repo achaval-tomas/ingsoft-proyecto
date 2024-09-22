4611833a6a2396b9e4f7cbaabc219e2aa0f1cf82
Nicolás Cagliero <nicolas.cagliero@mi.unc.edu.ar>
2024-09-21 22:55:29 -0300

    SCRUM-7 updates create_lobby function

--- src/routers/lobby.py
+++ src/routers/lobby.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI, APIRouter
+from pydantic import BaseModel
 
 from src.models.lobby import Lobby
 from  src.models.player import Player
 
 router = APIRouter()
 
+class Body(BaseModel):
+    name : str
+    min_players : int
+    max_players : int
+
 @router.post("/lobby")
-async def create_lobby(body : Lobby):
+async def create_lobby(body : Body):
 
     return body
