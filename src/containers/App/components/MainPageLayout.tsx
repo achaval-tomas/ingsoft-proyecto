02d4c474f96222b65d1d00db96c19d8d8595fc74
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-09-21 23:05:40 -0300

    use CreateLobbyDialog

--- src/containers/App/components/MainPageLayout.tsx
+++ src/containers/App/components/MainPageLayout.tsx
@@ -1,50 +1,31 @@
 import { useState } from "react";
 import FancyButton from "../../../components/FancyButton";
-import { LobbyForm } from "../containers/MainPage";
-import CreateLobbyForm from "./CreateLobbyForm";
+import CreateLobbyDialog, { CreateLobbyFormState } from "./CreateLobbyDialog";
 
 interface MainPageLayoutProps {
-    lobbyFormInputs: LobbyForm,
-    handleLobbyFormChange: (e: React.FormEvent<HTMLInputElement>) => void,
-    handleLobbyFormSubmit: React.FormEventHandler<HTMLFormElement>,
-    resetForm: () => void,
+    onSubmitLobbyForm: (state: CreateLobbyFormState) => void,
 }
 
 function MainPageLayout({ 
-    lobbyFormInputs,
-    handleLobbyFormSubmit,
-    handleLobbyFormChange,
-    resetForm,
+    onSubmitLobbyForm,
 }: MainPageLayoutProps) {
     const [showCreateForm, setCreateForm] = useState(false);
-
-    const handleGoBack = () => {
-        setCreateForm(false);
-        resetForm();
-    }
 
     return (
         <div className="w-screen flex justify-center">
             <div className="w-7/12 flex justify-around p-8 bg-zinc-700 rounded">
-                { showCreateForm ? 
-                    <>
-                        <CreateLobbyForm 
-                            inputs={lobbyFormInputs}
-                            handleChange={handleLobbyFormChange} 
-                            handleSubmit={handleLobbyFormSubmit} 
-                            handleGoBack={handleGoBack}
-                        />
-                    </>
-                    :
-                    <>
-                        <FancyButton onClick={() => setCreateForm(true)}>
-                            <p>Crear sala</p>
-                        </FancyButton>
-                        <FancyButton onClick={() => {}}>
-                            <p>Unirse a sala</p>
-                        </FancyButton>
-                    </>
-                }
+                <CreateLobbyDialog
+                    isOpen = {showCreateForm}
+                    lobbyNamePlaceholder = {"Nombre de tu sala"}
+                    onCancel = {() => setCreateForm(false)}
+                    onSubmit = {onSubmitLobbyForm}
+                />
+                <FancyButton onClick={() => setCreateForm(true)}>
+                    <p>Crear sala</p>
+                </FancyButton>
+                <FancyButton onClick={() => {}}>
+                    <p>Unirse a sala</p>
+                </FancyButton>
             </div>
         </div>
     );
--- src/containers/App/containers/MainPage.tsx
+++ src/containers/App/containers/MainPage.tsx
@@ -1,34 +1,18 @@
-import { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import { CreateLobbyFormState } from "../components/CreateLobbyDialog";
 
 export interface LobbyForm {
     name: string,
 }
 
-const initialInputs = {
-    name: "",
-};
-
 function MainPage() {
-    const [inputs, setInputs] = useState(initialInputs);
-
-    const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-        const name = e.currentTarget.name;
-        const value = e.currentTarget.value;
-        setInputs(inputs => ({...inputs, [name]: value}));
-    }
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log(`Chosen name: ${inputs.name}`);
+    const handleSubmit = (state: CreateLobbyFormState) => {
+        alert(JSON.stringify(state));
     }
 
     return (
         <MainPageLayout 
-            lobbyFormInputs={inputs}
-            handleLobbyFormChange={handleChange}
-            handleLobbyFormSubmit={handleSubmit}
-            resetForm={() => setInputs(initialInputs)}
+            onSubmitLobbyForm = {handleSubmit}
         /> 
     );
 }
