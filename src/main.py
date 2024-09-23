7a6b5c0a65ace66aeccbddb01705cf59ba5a6ab0
Santino Andres Quevedo Rodriguez <santino.quevedo@mi.unc.edu.ar>
2024-09-23 20:47:07 -0300

    SCRUM-14: Endpoint create player

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from src.routers.lobby import create_lobby_router
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(player.router)
 
 origins = [
     "http://localhost.tiangolo.com",
