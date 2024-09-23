243bd50ad4efed09a76055b51034273bd4257742
achaval-tomas <tomiachaval2013@gmail.com>
2024-09-23 16:07:24 -0300

    SCRUM-29 Create database models

---
+++ src/database/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+# from sqlalchemy.orm import relationship
+
+from src.database.db import Base
+
+class Player(Base):
+    __tablename__ = "players"
+    player_id = Column(String, primary_key=True)
+    player_name = Column(String)
+
+class Lobby(Base):
+    __tablename__ = "lobbies"
+    lobby_id = Column(String, primary_key=True)
+    lobby_name = Column(String)
+    lobby_owner = Column(String, ForeignKey("players.player_id"))
+    min_players = Column(Integer)
+    max_players = Column(Integer)
