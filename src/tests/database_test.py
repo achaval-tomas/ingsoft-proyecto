35953fd7a3aeda16eab0bfbe9c92a4d049ec6bea
achaval-tomas <tomiachaval2013@gmail.com>
2024-09-23 16:10:49 -0300

    SCRUM-29 Create database test
    Run with uvicorn src.tests.database_test:app --reload and go to /docs

---
+++ src/tests/database_test.py
@@ -0,0 +1,21 @@
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+from src.database import models, schemas, crud
+from src.database.session import get_db
+from src.database.db import engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.post("/lobby")
+def create_lobby(lobby: schemas.LobbyCreate, db: Session = Depends(get_db)):
+    # If there was the possibility to create duplicates, it should be checked
+    # here and handled with HTTP exceptions.
+    return crud.create_lobby(db=db, lobby=lobby)
+
+@app.post("/lobby/{lobby_id}/delete")
+def delete_lobby(lobby_id: str, db: Session = Depends(get_db)):
+    # If there was the possibility to create duplicates, it should be checked
+    # here and handled with HTTP exceptions.
+    return crud.delete_lobby(db=db,lobby_id=lobby_id)
