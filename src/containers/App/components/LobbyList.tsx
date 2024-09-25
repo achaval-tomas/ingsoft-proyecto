941e430c827dc28cf6891ce9ef4f579a8eb679d3
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-09-25 17:14:53 -0300

    SCRUM-9 switch from list to table

--- src/containers/App/components/LobbyList.tsx
+++ src/containers/App/components/LobbyList.tsx
@@ -11,24 +11,26 @@ interface LobbyListProps {
 
 export default function LobbyList({ lobbyList, joinHandler }: LobbyListProps) {
     const items = lobbyList.map(lobby =>
-        <li key={lobby.id} >
-            <div
-                className="flex justify-around my-2 p-2 border items-baseline"
-            >
-                <p><b>{lobby.name}</b></p>
-                <p>{lobby.numPlayers}</p>
+        <tr
+            className="flex justify-around my-2 p-2 border items-baseline"
+            key={lobby.id}
+        >
+            <td><b>{lobby.name}</b></td>
+            <td>{lobby.numPlayers}</td>
+            <td>
                 <button
                     className="p-1 rounded bg-primary-600 hover:bg-primary-500"
                     onClick={() => joinHandler(lobby.id)}
                 >
                     Unirse
                 </button>
-
-            </div>
-        </li>,
+            </td>
+        </tr>,
     );
 
-    return <ul className="w-screen">
-        {items}
-    </ul>;
+    return <table className="w-screen">
+        <tbody>
+            {items}
+        </tbody>
+    </table>;
 }
