1311fe8c1a4fa3cf8f3dfc077da4f38442a56214
Tomás Peyronel <tpeyronel@gmail.com>
2024-09-26 17:20:14 -0300

    SCRUM-6 add more CreateLobbyDialog tests

--- src/containers/App/components/CreateLobbyDialog.test.tsx
+++ src/containers/App/components/CreateLobbyDialog.test.tsx
@@ -3,16 +3,16 @@ import { expect, test, vitest } from "vitest";
 import { userEvent } from "@testing-library/user-event";
 import CreateLobbyDialog, { CreateLobbyFormState } from "./CreateLobbyDialog";
 
-test("correct state is returned", async () => {
-    const formInputs = {
-        name: "Hello World",
-        maxPlayers: 3,
-        password: "1234",
-    };
+type FormInput = {
+    name?: string;
+    maxPlayers?: string;
+    password?: string;
+}
 
+async function genericSuccessTest(input: FormInput, expectedOutput: CreateLobbyFormState) {
     const onCancel = vitest.fn();
     const onSubmit = vitest.fn((formState: CreateLobbyFormState) => {
-        expect(formState).toStrictEqual(formInputs);
+        expect(formState).toStrictEqual(expectedOutput);
     });
 
     render(
@@ -24,13 +24,63 @@ test("correct state is returned", async () => {
         />,
     );
 
-    await userEvent.type(screen.getByTestId("lobby-name"), formInputs.name);
-    await userEvent.clear(screen.getByTestId("lobby-max-players"));
-    await userEvent.type(screen.getByTestId("lobby-max-players"), formInputs.maxPlayers.toString());
-    await userEvent.type(screen.getByTestId("lobby-password"), formInputs.password);
+    if (input.name != null) {
+        await userEvent.type(screen.getByTestId("lobby-name"), input.name);
+    }
+
+    if (input.maxPlayers != null) {
+        await userEvent.clear(screen.getByTestId("lobby-max-players"));
+        await userEvent.type(screen.getByTestId("lobby-max-players"), input.maxPlayers);
+    }
+
+    if (input.password != null) {
+        await userEvent.type(screen.getByTestId("lobby-password"), input.password);
+    }
 
     await userEvent.click(screen.getByTestId("lobby-btn-create"));
 
     expect(onCancel).not.toHaveBeenCalled();
     expect(onSubmit).toHaveBeenCalledOnce();
+}
+
+test("correct state is returned", async () => {
+    const input = {
+        name: "Hello World",
+        maxPlayers: "3",
+        password: "1234",
+    };
+
+    const expectedOutput = {
+        name: "Hello World",
+        maxPlayers: 3,
+        password: "1234",
+    };
+
+    await genericSuccessTest(input, expectedOutput);
+});
+
+test("empty name and password work fine", async () => {
+    const input = {
+        maxPlayers: "3",
+    };
+
+    const expectedOutput = {
+        name: "",
+        maxPlayers: 3,
+        password: "",
+    };
+
+    await genericSuccessTest(input, expectedOutput);
+});
+
+test("maxPlayers defaults to 4", async () => {
+    const input = {};
+
+    const expectedOutput = {
+        name: "",
+        maxPlayers: 4,
+        password: "",
+    };
+
+    await genericSuccessTest(input, expectedOutput);
 });
