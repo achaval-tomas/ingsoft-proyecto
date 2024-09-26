68408fd34f5ea1cf45f91947bed7eab0c34b24f8
achaval-tomas <tomiachaval2013@gmail.com>
2024-09-26 15:08:09 -0300

    SCRUM-31 Unique ID generation
    Also minor fixes to crud

--- src/database/crud.py
+++ src/database/crud.py
@@ -1,29 +1,27 @@
 from sqlalchemy.orm import Session
 from src.database import models, schemas
-import random
+from uuid import uuid4
 
-# TO-DO: Unique ID generation
-
-''' READING METHODS '''
+''' READ METHODS '''
 def get_player(db: Session, player_id: str):
-    return db.query(models.Player).filter(models.Player.player_id == player_id)
+    return db.query(models.Player).filter(models.Player.player_id == player_id).one_or_none()
 
 def get_lobby(db: Session, lobby_id: str):
-    return db.query(models.Lobby).filter(models.Lobby.lobby_id == lobby_id)
+    return db.query(models.Lobby).filter(models.Lobby.lobby_id == lobby_id).one_or_none()
 
 def get_lobby_list(db: Session, limit: int = 1000):
     return db.query(models.Lobby).all()
 
-''' WRITING METHODS '''
+''' WRITE METHODS '''
 def create_player(db: Session, player: schemas.PlayerCreate):
-    db_player = models.Player(player_name=player.player_name, player_id='1234')
+    db_player = models.Player(player_name=player.player_name, player_id=str(uuid4()))
     db.add(db_player)
     db.commit()
     db.refresh(db_player)
     return db_player.player_id
 
 def create_lobby(db: Session, lobby: schemas.LobbyCreate):
-    db_lobby = models.Lobby(lobby_id=str(int(random.random()*1000)),
+    db_lobby = models.Lobby(lobby_id=str(uuid4()),
                             lobby_name=lobby.lobby_name,
                             lobby_owner=lobby.lobby_owner,
                             min_players=lobby.min_players,
@@ -34,7 +32,7 @@ def create_lobby(db: Session, lobby: schemas.LobbyCreate):
     db.refresh(db_lobby)
     return db_lobby.lobby_id
 
-''' DELETING METHODS '''
+''' DELETE METHODS '''
 def delete_player(db: Session, player_id: str):
     db.query(models.Player).filter(models.Player.player_id == player_id).delete()
     db.commit()
