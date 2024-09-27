2b48b9e96a7fe208b472a2acdf5f59de92c5ecec
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-09-27 17:05:14 -0300

    SCRUM-8 show error when server error

--- src/containers/App/containers/InitialPage.tsx
+++ src/containers/App/containers/InitialPage.tsx
@@ -1,29 +1,42 @@
 import { useNavigate } from "react-router-dom";
 import PlayerNameForm from "../components/PlayerNameForm";
+import { useState } from "react";
 
 function InitialPage() {
+    const [ serverError, setServerError ] = useState<boolean>(false);
     const navigate = useNavigate();
 
     async function createPlayer(playerName: string) {
-        const res = await fetch("http://127.0.0.1:8000/player", {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                player_name: playerName,
-            }),
-        });
+        try {
+            const res = await fetch("http://127.0.0.1:8000/player", {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    player_name: playerName,
+                }),
+            });
 
-        const data = await res.json() as { player_id: string };
+            const data = await res.json() as { player_id: string };
 
-        navigate(`lobby?player=${data.player_id}`);
+            navigate(`lobby?player=${data.player_id}`);
+        } catch {
+            setServerError(true);
+        }
     }
 
     return <div className="flex flex-col align-center w-screen px-16">
         <h1 className="my-16 text-center">El Switcher</h1>
         <PlayerNameForm handleSubmit={name => void createPlayer(name)} />
+        { serverError &&
+            <p
+                className="text-center text-red-500 py-2"
+            >
+                Hay un error en el servidor, por favor intentá más tarde
+            </p>
+        }
     </div>;
 }
 
