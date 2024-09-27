14a987e05e6f8d8e27501dec2d3a957095cb2f7d
Tomás Peyronel <tpeyronel@gmail.com>
2024-09-27 09:51:18 -0300

    SCRUM-34 basic react-router use

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,32 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./containers/App/App.tsx";
 import "./index.css";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import CreateLobbyDialog from "./containers/App/components/CreateLobbyDialog.tsx";
+import ErrorPage from "./containers/ErrorPage/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/play",
+        element: <CreateLobbyDialog
+            isOpen = {true}
+            lobbyNamePlaceholder = {"Nombre de tu sala"}
+            onCancel = {() => {}}
+            onSubmit = {() => {}}
+        />,
+    },
+]);
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 );
