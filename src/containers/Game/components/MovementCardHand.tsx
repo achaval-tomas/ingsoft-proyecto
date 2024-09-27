d4572e18068889e16577cec032aa886c1f3ff668
Tomás Peyronel <tpeyronel@gmail.com>
2024-09-27 16:08:21 -0300

    SCRUM-21 improve movement hand ui

--- src/containers/Game/components/MovementCardHand.tsx
+++ src/containers/Game/components/MovementCardHand.tsx
@@ -10,17 +10,17 @@ function MovementCardHand({ movements }: MovementCardHandProps) {
     return (
         <div className="flex flex-row px-[1.4em] pb-[1.8em]">
             <div className="group">
-                <div className="relative top-[1em] -rotate-[15deg] group-hover:top-[-2em] group-hover:z-30 transition-[top]">
+                <div className="relative top-[1em] -rotate-[15deg] group-hover:top-[-1.5em] group-hover:z-30 transition-[top]">
                     <MovementCard movement={movements[0]}/>
                 </div>
             </div>
             <div className="group">
-                <div className="relative z-10 group-hover:top-[-2em] group-hover:z-30 transition-[top]">
+                <div className="relative top-[0em] z-10 group-hover:top-[-2em] group-hover:z-30 transition-[top]">
                     <MovementCard movement={movements[1]}/>
                 </div>
             </div>
             <div className="group">
-                <div className="relative top-[1em] rotate-[15deg] z-20 group-hover:top-[-2em] group-hover:z-30 transition-[top]">
+                <div className="relative top-[1em] rotate-[15deg] z-20 group-hover:top-[-1.5em] group-hover:z-30 transition-[top]">
                     <MovementCard movement={movements[2]}/>
                 </div>
             </div>
