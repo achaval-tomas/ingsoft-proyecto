0a94859aec33ec3c57facd1f3707ddf3b4a33c4a
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-09-27 16:39:49 -0300

    SCRUM-8 refactor form disabilitation when input empty

--- src/containers/App/components/PlayerNameForm.tsx
+++ src/containers/App/components/PlayerNameForm.tsx
@@ -9,13 +9,18 @@ interface PlayerNameFormProps {
 
 function PlayerNameForm({ handleSubmit }: PlayerNameFormProps) {
     const [ name, setName ] = useState<string>("");
-    const submitDisabled = name === "";
+    const [ showWarning, setShowWarning ] = useState<boolean>(false);
 
     return (
         <form
             onSubmit={(e) => {
                 e.preventDefault();
-                handleSubmit(name);
+
+                if (name !== "")
+                    handleSubmit(name);
+                else {
+                    setShowWarning(true);
+                }
             }}
             className="border border-surface-500 rounded p-6 flex flex-col justify-center items-center"
         >
@@ -25,14 +30,18 @@ function PlayerNameForm({ handleSubmit }: PlayerNameFormProps) {
                     type="text"
                     id="player-name"
                     value={name || ""}
-                    onChange={e => setName(e.target.value)}
+                    onChange={e => {
+                        setName(e.target.value);
+                        setShowWarning(false);
+                    }}
                     className="col-span-1 row-span-1"
                 >
                 </input>
-                { submitDisabled && <p className="text-sm col-start-2 !text-red-400">Elegí un nombre</p>}
+                { showWarning && <p className="text-sm col-start-2 !text-red-400">Elegí un nombre</p>}
             </div>
             <FilledButton
-                className={"mt-4 " + (submitDisabled ? "!bg-surface-500" : "")}
+                className="mt-4"
+                backgroundColor={showWarning ? "bg-red-600" : undefined}
                 type="submit"
             >
                 Crear
