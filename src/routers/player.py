3079b54c6368307e92bc2140e441aba61d01c168
achaval-tomas <tomiachaval2013@gmail.com>
2024-09-27 14:53:11 -0300

    SCRUM-14 Correcciones al router para crear jugador
    Co-authored-by: Santino Quevedo <santino.quevedo@mi.unc.edu.ar>

--- src/routers/player.py
+++ src/routers/player.py
@@ -1,22 +1,14 @@
-from fastapi import FastAPI, APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from src.database.schemas import PlayerCreate
 from src.database import crud
 from src.database.session import get_db
 
-app = FastAPI()
-router = APIRouter()
+create_player_router = APIRouter()
 
-
-
-@router.post("/player", status_code=201)
+@create_player_router.post("/player", status_code=201)
 async def create_player(player: PlayerCreate, db: Session = Depends(get_db)):
-    
     if not player.player_name:
         raise HTTPException(status_code=400, detail="Player name cannot be empty")
     
-
-    new_player = crud.create_player(db = db,  player = player)
-    
-    
-    return {"message": "Player created successfully", "player_id": new_player}
+    return {"player_id": crud.create_player(db = db, player = player)}
