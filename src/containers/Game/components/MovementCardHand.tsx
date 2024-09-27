8721035aa1608585cfa1844babbc87f34566b0e5
Tomás Peyronel <tpeyronel@gmail.com>
2024-09-27 11:28:13 -0300

    SCRUM-18 hover animation for movement cards

--- src/containers/Game/components/MovementCardHand.tsx
+++ src/containers/Game/components/MovementCardHand.tsx
@@ -9,9 +9,9 @@ function MovementCardHand({ movements }: MovementCardHandProps) {
 
     return (
         <div className="flex flex-row px-[1.4em] pb-[1.8em]">
-            <div className="relative top-[1em] -rotate-[15deg]"><MovementCard movement={movements[0]}/></div>
-            <div className="relative z-10"><MovementCard movement={movements[1]}/></div>
-            <div className="relative top-[1em] rotate-[15deg] z-20"><MovementCard movement={movements[2]}/></div>
+            <div className="relative top-[1em] -rotate-[15deg] hover:top-[-2em] hover:z-30 transition-[top]"><MovementCard movement={movements[0]}/></div>
+            <div className="relative z-10 hover:top-[-2em] hover:z-30 transition-[top]"><MovementCard movement={movements[1]}/></div>
+            <div className="relative top-[1em] rotate-[15deg] z-20 hover:top-[-2em] hover:z-30 transition-[top]"><MovementCard movement={movements[2]}/></div>
         </div>
     );
 }
