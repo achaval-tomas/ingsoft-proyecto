d35ee4914fc8932ed50c61678fda05256fa3e13c
Tomás Peyronel <tpeyronel@gmail.com>
2024-09-28 15:38:32 -0300

    SCRUM-21 fix small graphical bug

--- src/containers/Game/components/MovementCardHand.tsx
+++ src/containers/Game/components/MovementCardHand.tsx
@@ -10,17 +10,17 @@ function MovementCardHand({ movements }: MovementCardHandProps) {
     return (
         <div className="flex flex-row px-[1.4em] pb-[1.8em]">
             <div className="group">
-                <div className="relative top-[1em] group-hover:top-[-1em] group-hover:z-20 shadow-lg shadow-black hover:shadow-xl hover:shadow-black transition-movement-card z-[10] -rotate-[15deg]">
+                <div className="rounded relative top-[1em] group-hover:top-[-1em] group-hover:z-20 shadow-lg shadow-black hover:shadow-xl hover:shadow-black transition-movement-card z-[10] -rotate-[15deg]">
                     <MovementCard movement={movements[0]}/>
                 </div>
             </div>
             <div className="group">
-                <div className="relative top-[0em] group-hover:top-[-2em] group-hover:z-20 shadow-lg shadow-black hover:shadow-xl hover:shadow-black transition-movement-card z-[11]">
+                <div className="rounded relative top-[0em] group-hover:top-[-2em] group-hover:z-20 shadow-lg shadow-black hover:shadow-xl hover:shadow-black transition-movement-card z-[11]">
                     <MovementCard movement={movements[1]}/>
                 </div>
             </div>
             <div className="group">
-                <div className="relative top-[1em] group-hover:top-[-1em] group-hover:z-20 shadow-lg shadow-black hover:shadow-xl hover:shadow-black transition-movement-card z-[12] rotate-[15deg]">
+                <div className="rounded relative top-[1em] group-hover:top-[-1em] group-hover:z-20 shadow-lg shadow-black hover:shadow-xl hover:shadow-black transition-movement-card z-[12] rotate-[15deg]">
                     <MovementCard movement={movements[2]}/>
                 </div>
             </div>
