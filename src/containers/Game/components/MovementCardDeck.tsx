1baefd67dba5fe84bf845945936242a590fdb2c7
Tomás Peyronel <tpeyronel@gmail.com>
2024-09-28 20:02:30 -0300

    SCRUM-21 add and use backside.svg

--- src/containers/Game/components/MovementCardDeck.tsx
+++ src/containers/Game/components/MovementCardDeck.tsx
@@ -2,20 +2,20 @@ function MovementCardDeck() {
     return (
         <div className="relative">
             <img
-                src="/src/assets/movements/backside.jpg"
-                className="relative w-[8em] h-[12em] shadow-sm shadow-black z-30"
+                src="/src/assets/movements/backside.svg"
+                className="relative rounded-xl w-[8em] h-[12em] shadow-sm shadow-black z-30"
             />
             <img
-                src="/src/assets/movements/backside.jpg"
-                className="absolute top-[2px] right-[2px] w-[8em] h-[12em] shadow-sm shadow-black z-20"
+                src="/src/assets/movements/backside.svg"
+                className="absolute rounded-xl top-[2px] right-[2px] w-[8em] h-[12em] shadow-sm shadow-black z-20"
             />
             <img
-                src="/src/assets/movements/backside.jpg"
-                className="absolute top-[4px] right-[4px] w-[8em] h-[12em] shadow-sm shadow-black z-10"
+                src="/src/assets/movements/backside.svg"
+                className="absolute rounded-xl top-[4px] right-[4px] w-[8em] h-[12em] shadow-sm shadow-black z-10"
             />
             <img
-                src="/src/assets/movements/backside.jpg"
-                className="absolute top-[6px] right-[6px] w-[8em] h-[12em] shadow-md shadow-black z-0"
+                src="/src/assets/movements/backside.svg"
+                className="absolute rounded-xl top-[6px] right-[6px] w-[8em] h-[12em] shadow-md shadow-black z-0"
             />
         </div>
     );
