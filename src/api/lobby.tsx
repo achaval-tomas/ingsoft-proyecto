703739ef271e5f23833127addf1fc1ea62da7bae
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-09-29 15:59:08 -0300

    SCRUM-4 add join lobby function

--- src/api/lobby.tsx
+++ src/api/lobby.tsx
@@ -37,4 +37,48 @@ async function createLobby(
     return data.lobby_id;
 }
 
-export { getJoinableLobbies, createLobby };
+type joinLobbyResponse = {
+    goHome: boolean;
+    errorMsg: string;
+}
+
+async function joinLobby(playerId: string, lobbyId: string): Promise<joinLobbyResponse> {
+    const res = await fetch("http://127.0.0.1:8000/lobby/join", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            player_id: playerId,
+            lobby_id: lobbyId,
+        }),
+    });
+
+    console.log(res);
+
+    if (res.ok)
+        return { goHome: false, errorMsg: "" };
+
+    const json = await res.json() as { detail: string };
+
+    if (res.status === 400 && json.detail === "Lobby is full") {
+        return { goHome: false, errorMsg: "Esta sala está llena." };
+    }
+
+    if (res.status === 400 && json.detail === "Already joined") {
+        return { goHome: false, errorMsg: "El jugador ya está en esta sala." };
+    }
+
+    if (res.status === 404 && json.detail === "Player not found") {
+        return { goHome: true, errorMsg: "El jugador no existe." };
+    }
+
+    if (res.status === 404 && json.detail === "Lobby not found") {
+        return { goHome: false, errorMsg: "Esta sala no existe." };
+    }
+
+    return { goHome: false, errorMsg: "Error en el servidor." };
+}
+
+export { getJoinableLobbies, createLobby, joinLobby };
