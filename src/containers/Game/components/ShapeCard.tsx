26fdb17c04457600b731db4f746632d3ae0942bc
achaval-tomas <tomiachaval2013@gmail.com>
2024-09-29 14:02:02 -0300

    SCRUM-19 Create ShapeCard component

---
+++ src/containers/Game/components/ShapeCard.tsx
@@ -0,0 +1,13 @@
+import { Shape } from "../../../domain/Shape";
+
+type ShapeCardProps = {
+    shape: Shape;
+}
+
+function ShapeCard({ shape }: ShapeCardProps) {
+    const imageSrc = `/src/assets/shapes/${shape}.svg`;
+
+    return <img src={imageSrc} className="rounded w-[6em] h-[6em]" />
+}
+
+export default ShapeCard;
