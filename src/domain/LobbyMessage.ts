0a974b2585c467e71530100ea2dc65c154b07ec1
Tomás Peyronel <tpeyronel@gmail.com>
2024-09-29 17:00:58 -0300

    SCRUM-37 change LobbyMessageIn

--- src/domain/LobbyMessage.ts
+++ src/domain/LobbyMessage.ts
@@ -4,15 +4,13 @@ import { PlayerIdSchema } from "./GameState";
 // Messages received from the backend
 export const LobbyMessageInSchema = z.discriminatedUnion("type", [
     z.object({
-        type: z.literal("player-joined"), // Received when someone has joined.
-        player: z.object({
-            id: PlayerIdSchema,
-            name: z.string(),
-        }),
-    }),
-    z.object({
-        type: z.literal("player-left"), // Received when someone has left.
-        playerId: PlayerIdSchema, // ID of the player who has left.
+        type: z.literal("player-list"), // Received when someone has joined.
+        players: z
+            .object({
+                id: PlayerIdSchema,
+                name: z.string(),
+            })
+            .array(),
     }),
     z.object({
         type: z.literal("game-started"), // Received when the owner has started the game.
