1ed991bcffb7134f1796e480febc000e6e3cc685
achaval-tomas <tomiachaval2013@gmail.com>
2024-09-30 18:32:07 -0300

    SCRUM-39 Improve comments

--- src/routers/helpers/connection_manager.py
+++ src/routers/helpers/connection_manager.py
@@ -8,25 +8,25 @@ class ConnectionManager:
     def __init__(self):
         self.active_connections: dict[str, WebSocket] = {}
 
-    # accept websocket connection, added to active_connections
+    # accepts a websocket connection, saves it in active_connections
     async def connect(self, websocket: WebSocket, player_id: str):
         await websocket.accept()
         if player_id in self.active_connections.keys():
             self.disconnect(player_id=player_id)
         self.active_connections[player_id] = websocket
 
-    # remove from list of connections
+    # removes a disconnected player from list of active connections
     def disconnect(self, player_id: str):
         del self.active_connections[player_id]
 
-    # send message through a specific websocket
+    # sends a message to a specific player
     async def send_personal_message(self, message: str, player_id: str):
         if player_id not in self.active_connections.keys():
             return
         await self.active_connections[player_id].send_text(message)
 
 class GameConnectionManager(ConnectionManager):
-    # broadcast a message to everyone (currently connected) in game #game_id
+    # broadcasts a message to everyone (currently connected) in a game
     async def broadcast_in_game(self, message: str, db: Session, game_id: int):
         game_players = crud_game.get_game_players(db=db, game_id=game_id)
         connected_players = [player for player in game_players if player in self.active_connections.keys()]
@@ -34,7 +34,7 @@ class GameConnectionManager(ConnectionManager):
             await self.send_personal_message(message=message, player_id=player)
             
 class LobbyConnectionManager(ConnectionManager):
-    # broadcast a message to everyone (currently connected) in game #game_id
+    # broadcasts a message to everyone (currently connected) in a lobby
     async def broadcast_in_lobby(self, message: str, db: Session, lobby_id: int):
         lobby = get_lobby(db=db, lobby_id=lobby_id)
         if not lobby:
