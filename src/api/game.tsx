382f1f241ceb5cf521cb79b464541dd4fd57c82f
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-09-30 11:32:44 -0300

    SCRUM-26 create game api function

---
+++ src/api/game.tsx
@@ -0,0 +1,43 @@
+type CreateGameResult =
+    | "Sala no encontrada"
+    | "Jugador no encontrado"
+    | "Ok"
+    | "Solo el dueño de la sala puede iniciarlo"
+
+async function createGame(
+    playerId: string,
+    lobbyId: string,
+): Promise<CreateGameResult | null> {
+    const res = await fetch("http://127.0.0.1:8000/game", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            player_id: playerId,
+            lobby_id: lobbyId,
+        }),
+    });
+
+    if (res.ok)
+        return "Ok";
+
+    const json = await res.json() as { detail: string };
+
+    if (res.status === 404 && json.detail === "Player not found") {
+        return "Jugador no encontrado";
+    }
+
+    if (res.status === 404 && json.detail === "Lobby not found") {
+        return "Sala no encontrada";
+    }
+
+    if (res.status === 400) {
+        return "Solo el dueño de la sala puede iniciarlo";
+    }
+
+    return null;
+}
+
+export { createGame };
