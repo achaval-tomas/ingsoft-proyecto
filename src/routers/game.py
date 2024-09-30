5dd9b571bd7e40099bcb00ba124cf616d1cc44ad
achaval-tomas <tomiachaval2013@gmail.com>
2024-09-30 12:33:31 -0300

    SCRUM-17 Match new request types from frontend

--- src/routers/game.py
+++ src/routers/game.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, APIRouter, HTTPException, WebSocket, WebSocketDisconnect
 from sqlalchemy.orm import Session
 from src.database import schemas
+from src.database.crud.tools.jsonify import deserialize
 from src.database.session import get_db
 from src.routers.handlers.ws_handle_endturn import ws_handle_endturn
 from src.routers.handlers.ws_handle_game_start import ws_handle_game_start
@@ -43,8 +44,9 @@ async def game_websocket(player_id: str, ws: WebSocket, db: Session = Depends(ge
     try:
         while True:
             response = ""
-            request = await ws.receive_text()
-            match request:
+            received = await ws.receive_text()
+            request = deserialize(received)
+            match request['type']:
                 case 'get-game-state':
                     response = ws_handle_gamestate(player_id=player_id, db=db)
                 case 'end-turn':
