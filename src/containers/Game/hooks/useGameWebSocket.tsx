39889b97f8eabcf45a824f2633b5364f9c31cd6c
Tomás Peyronel <tpeyronel@gmail.com>
2024-10-05 19:49:05 -0300

    SCRUM-77 implement automatic websocket recreation

--- src/containers/Game/hooks/useGameWebSocket.tsx
+++ src/containers/Game/hooks/useGameWebSocket.tsx
@@ -1,50 +1,61 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { PlayerId } from "../../../domain/GameState";
 import { wsServerUrl } from "../../../services/config";
 import { GameMessageIn, GameMessageInSchema, GameMessageOut } from "../../../domain/GameMessage";
+
+function createWebSocket(
+    playerId: PlayerId,
+    onReceive: (msg: GameMessageIn) => void,
+): WebSocket {
+    const ws = new WebSocket(`${wsServerUrl}/game/${playerId}`);
+
+    ws.addEventListener("message", e => {
+        if (typeof e.data !== "string") {
+            console.error("invalid message type");
+            console.error(e.data);
+            return;
+        }
+
+        let message: GameMessageIn;
+        try {
+            message = GameMessageInSchema.parse(JSON.parse(e.data));
+        } catch {
+            console.error("invalid message object");
+            console.error(e.data);
+            return;
+        }
+
+        console.log(message);
+        onReceive(message);
+    });
+
+    return ws;
+}
 
 function useGameWebSocket(
     playerId: PlayerId,
     onReceive: (msg: GameMessageIn) => void,
 ): (msg: GameMessageOut) => void {
+    const [wsGeneration, setWsGeneration] = useState(0); // Used for triggering recreation of the websocket
     const wsRef = useRef<WebSocket>();
 
     useEffect(() => {
-        const ws = new WebSocket(`${wsServerUrl}/game/${playerId}`);
+        const ws = createWebSocket(playerId, onReceive);
         wsRef.current = ws;
-
-        ws.addEventListener("message", e => {
-            if (typeof e.data !== "string") {
-                console.error("invalid message type");
-                console.error(e.data);
-                return;
-            }
-
-            let message: GameMessageIn;
-            try {
-                message = GameMessageInSchema.parse(JSON.parse(e.data));
-            } catch {
-                console.error("invalid message object");
-                console.error(e.data);
-                return;
-            }
-
-            console.log(message);
-            onReceive(message);
-        });
-
         return () => ws.close();
-    }, [playerId, onReceive]);
+    }, [playerId, onReceive, wsGeneration]);
 
-    return (msg) => {
+    const sendMessage = useCallback((msg: GameMessageOut) => {
         try {
             wsRef.current?.send(JSON.stringify(msg));
         } catch (error) {
             console.error("error when sending game message");
             console.error(error);
-            // TODO: recreate ws
+            setWsGeneration(g => g + 1);
         }
-    };
+    }, []);
+
+    return sendMessage;
 }
 
 export default useGameWebSocket;
