de01d832b51412e022aa52081f8f2a3a6bee6e5a
Tomás Peyronel <tpeyronel@gmail.com>
2024-10-06 11:08:13 -0300

    SCRUM-77 avoid losing unset packages with game websocket

--- src/containers/Game/hooks/useGameWebSocket.tsx
+++ src/containers/Game/hooks/useGameWebSocket.tsx
@@ -5,6 +5,7 @@ import { GameMessageIn, GameMessageInSchema, GameMessageOut } from "../../../dom
 
 function createWebSocket(
     playerId: PlayerId,
+    onOpen: (ws: WebSocket) => void,
     onReceive: (msg: GameMessageIn) => void,
 ): WebSocket {
     const ws = new WebSocket(`${wsServerUrl}/game/${playerId}`);
@@ -25,32 +26,78 @@ function createWebSocket(
             return;
         }
 
-        console.log(message);
+        console.log("rx: ", message);
         onReceive(message);
+    });
+
+    ws.addEventListener("open", () => {
+        onOpen(ws);
+    });
+
+    ws.addEventListener("close", () => {
+        // TODO: onClose for reconnecting automatically
     });
 
     return ws;
 }
 
+function tryFlushQueue(ws: WebSocket, messageQueue: GameMessageOut[]) {
+    if (ws.readyState !== WebSocket.OPEN) {
+        return;
+    }
+
+    while (messageQueue.length !== 0) {
+        if (ws.readyState !== WebSocket.OPEN) {
+            return;
+        }
+
+        const message = messageQueue[0];
+        const messageString = JSON.stringify(message);
+
+        console.log("tx: ", message);
+
+        try {
+            ws.send(messageString);
+        } catch (error) {
+            console.error("error sending ws message");
+            console.error(error);
+            return;
+        }
+
+        messageQueue.shift();
+    }
+}
+
+
 function useGameWebSocket(
     playerId: PlayerId,
     onReceive: (msg: GameMessageIn) => void,
 ): (msg: GameMessageOut) => void {
-    const [wsGeneration, setWsGeneration] = useState(0); // Used for triggering recreation of the websocket
-    const wsRef = useRef<WebSocket>();
+    const messageQueueRef = useRef<GameMessageOut[]>([]);
+    const wsRef = useRef<WebSocket | null>(null);
+
+    // Used for triggering recreation of the websocket
+    const [wsGeneration, setWsGeneration] = useState(0);
 
     useEffect(() => {
-        const ws = createWebSocket(playerId, onReceive);
+        const ws = createWebSocket(playerId, (ws) => tryFlushQueue(ws, messageQueueRef.current), onReceive);
         wsRef.current = ws;
         return () => ws.close();
     }, [playerId, onReceive, wsGeneration]);
 
     const sendMessage = useCallback((msg: GameMessageOut) => {
-        try {
-            wsRef.current?.send(JSON.stringify(msg));
-        } catch (error) {
-            console.error("error when sending game message");
-            console.error(error);
+        const ws = wsRef.current;
+        const messageQueue = messageQueueRef.current;
+
+        messageQueue.push(msg);
+
+        if (ws == null) {
+            return;
+        }
+
+        tryFlushQueue(ws, messageQueue);
+
+        if (messageQueue.length !== 0 && ws.readyState !== WebSocket.CONNECTING) {
             setWsGeneration(g => g + 1);
         }
     }, []);
