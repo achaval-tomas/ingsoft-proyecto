2c68b6cc80b7f93a0acd255c29c2bc4b44911e4f
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-10-07 11:27:18 -0300

    SCRUM-96 get movement data from Movement

--- src/domain/Movement.ts
+++ src/domain/Movement.ts
@@ -10,9 +10,29 @@ export const MovementSchema = z.enum([
     "l-cw",
 ]);
 
+
 export type Movement = z.infer<typeof MovementSchema>;
 
 export type MovementData = {
     clamps: boolean;
     target: [number, number];
 };
+
+export function getMovementData(movement: Movement): MovementData {
+    switch (movement) {
+        case "straight-adjacent":
+            return { clamps: false, target: [1, 0] };
+        case "straight-spaced":
+            return { clamps: false, target: [2, 0] };
+        case "straight-edge":
+            return { clamps: true, target: [5, 0] };
+        case "diagonal-adjacent":
+            return { clamps: false, target: [1, 1] };
+        case "diagonal-spaced":
+            return { clamps: false, target: [2, 2] };
+        case "l-ccw":
+            return { clamps: false, target: [2, 1] };
+        case "l-cw":
+            return { clamps: false, target: [1, 2] };
+    }
+}
