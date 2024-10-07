a9633fcb012d898fa02db3de94aff967bb11268e
Santino Andres Quevedo Rodriguez <santino.quevedo@mi.unc.edu.ar>
2024-10-07 14:35:47 -0300

    SCRUM-52: Finishing remaining tests

--- src/tests/endpoints_tests.py
+++ src/tests/endpoints_tests.py
@@ -1,9 +1,13 @@
 from fastapi.testclient import TestClient
 
+from src.constants import errors
+from src.database.crud.crud_player import delete_player
+from src.database.crud.crud_game import delete_game, get_game
+from src.database.session import get_db
 from src.main import app
 from src.schemas.game_schemas import GameCreate
-from src.schemas.lobby_schemas import LobbyCreate
-from src.schemas.player_schemas import PlayerCreate
+from src.schemas.lobby_schemas import LobbyCreateSchema
+from src.schemas.player_schemas import PlayerCreateSchema
 
 client = TestClient(app)
 
@@ -11,7 +15,7 @@ client = TestClient(app)
 
 
 def test_create_player():
-    player_data = PlayerCreate(player_name='Test Player')
+    player_data = PlayerCreateSchema(player_name='Test Player')
     response = client.post('/player', json=player_data.model_dump())
 
     assert response.status_code == 201
@@ -21,7 +25,7 @@ def test_create_player():
 
 
 def test_create_player_br():
-    player_data = PlayerCreate(player_name='')
+    player_data = PlayerCreateSchema(player_name='')
     response = client.post('/player', json=player_data.model_dump())
 
     assert response.status_code == 400
@@ -34,7 +38,7 @@ def test_create_player_br():
 
 
 def test_create_game():
-    player_test = PlayerCreate(player_name='TestGame')
+    player_test = PlayerCreateSchema(player_name='TestGame')
     player_test_id = client.post('/player', json=player_test.model_dump())
 
     player_test_json = player_test_id.json()
@@ -43,7 +47,7 @@ def test_create_game():
     ), "El 'player_id' no se encuentra en la respuesta del servidor"
     player_id = player_test_json['player_id']
 
-    lobby_test = LobbyCreate(
+    lobby_test = LobbyCreateSchema(
         lobby_name='LobbyTest',
         lobby_owner=player_id,
         min_players=0,
@@ -62,6 +66,310 @@ def test_create_game():
     response = client.post('/game', json=game_test.model_dump())
 
     assert response.status_code == 200
+
+
+def test_start_game_error_lobby_not_found():
+    player_test = PlayerCreateSchema(player_name='Santino')
+    player_response = client.post('/player', json=player_test.model_dump())
+    player_test_json = player_response.json()
+    assert (
+        'player_id' in player_test_json
+    ), "El 'player_id' no se encuentra en la respuesta del servidor"
+    player_id = player_test_json['player_id']
+
+    lobby_test = LobbyCreateSchema(
+        lobby_name='LobbyTest',
+        lobby_owner=player_id,
+        min_players=0,
+        max_players=4,
+    )
+    lobby_response = client.post('/lobby', json=lobby_test.model_dump())
+    assert lobby_response.status_code == 200, 'No se pudo crear el lobby'
+    lobby_test_json = lobby_response.json()
+
+    assert (
+        'lobby_id' in lobby_test_json
+    ), "El 'lobby_id' no se encuentra en la respuesta del servidor"
+    lobby_id = lobby_test_json['lobby_id']
+
+    fake_lobby_id = '-1'
+
+    game_test = GameCreate(lobby_id=fake_lobby_id, player_id=player_id)
+    response = client.post('/game', json=game_test.model_dump())
+
+    assert (
+        response.status_code == 404
+    ), f'Se esperaba un status code 404, pero se recibió {response.status_code}'
+    assert (
+        response.json()['detail'] == errors.LOBBY_NOT_FOUND
+    ), f"Mensaje de error incorrecto: {response.json()['detail']}"
+
+
+def test_start_game_error_you_should_be_a_owner():
+    player_owner = PlayerCreateSchema(player_name='Santino')
+    player_response = client.post('/player', json=player_owner.model_dump())
+    player_owner_json = player_response.json()
+    assert (
+        'player_id' in player_owner_json
+    ), "El 'player_id' no se encuentra en la respuesta del servidor"
+    player_owner_id = player_owner_json['player_id']
+
+    player_fake_owner = PlayerCreateSchema(player_name='Cande')
+    player_response = client.post('/player', json=player_fake_owner.model_dump())
+    player_fake_owner_json = player_response.json()
+    assert (
+        'player_id' in player_fake_owner_json
+    ), "El 'player_id' no se encuentra en la respuesta del servidor"
+    player_fake_owner_id = player_fake_owner_json['player_id']
+
+    lobby_test = LobbyCreateSchema(
+        lobby_name='LobbyTest',
+        lobby_owner=player_owner_id,
+        min_players=0,
+        max_players=4,
+    )
+    lobby_response = client.post('/lobby', json=lobby_test.model_dump())
+    assert lobby_response.status_code == 200, 'No se pudo crear el lobby'
+    lobby_test_json = lobby_response.json()
+
+    assert (
+        'lobby_id' in lobby_test_json
+    ), "El 'lobby_id' no se encuentra en la respuesta del servidor"
+    lobby_id = lobby_test_json['lobby_id']
+
+    game_test = GameCreate(lobby_id=lobby_id, player_id=player_fake_owner_id)
+    response = client.post('/game', json=game_test.model_dump())
+
+    assert (
+        response.status_code == 400
+    ), f'Se esperaba un status code 400, pero se recibió {response.status_code}'
+    assert (
+        response.json()['detail'] == errors.MUST_BE_OWNER
+    ), f"Mensaje de error incorrecto: {response.json()['detail']}"
+
+
+def test_start_game_error_not_enough_players():
+    player_test = PlayerCreateSchema(player_name='Santino')
+    player_test_id = client.post('/player', json=player_test.model_dump())
+    player_test_json = player_test_id.json()
+    assert (
+        'player_id' in player_test_json
+    ), "El 'player_id' no se encuentra en la respuesta del servidor"
+    player_id = player_test_json['player_id']
+
+    lobby_test = LobbyCreateSchema(
+        lobby_name='LobbyTest',
+        lobby_owner=player_id,
+        min_players=2,
+        max_players=4,
+    )
+    lobby_test_id = client.post('/lobby', json=lobby_test.model_dump())
+
+    lobby_json = lobby_test_id.json()
+    assert (
+        'lobby_id' in lobby_json
+    ), "El 'lobby_id' no se encuentra en la respuesta del servidor"
+    lobby_id = lobby_json['lobby_id']
+
+    data = {
+        'player_id': player_id,
+        'lobby_id': lobby_id,
+    }
+
+    client.post('lobby/join', json=data)
+
+    game_test = GameCreate(lobby_id=lobby_id, player_id=player_id)
+    response = client.post('/game', json=game_test.model_dump())
+    assert (
+        response.status_code == 400
+    ), f'Se esperaba un status code 400, pero se recibió {response.status_code}'
+    assert (
+        response.json()['detail'] == errors.NOT_ENOUGH_PLAYERS
+    ), f"Mensaje de error incorrecto: {response.json()['detail']}"
+
+
+def test_start_game_error_player_is_missing():
+    player_test_1 = PlayerCreateSchema(player_name='Santino')
+    player_test_1_id = client.post('/player', json=player_test_1.model_dump())
+    player_test_1_json = player_test_1_id.json()
+    assert (
+        'player_id' in player_test_1_json
+    ), "El 'player_id' no se encuentra en la respuesta del servidor"
+    player_id_1 = player_test_1_json['player_id']
+
+    player_test_2 = PlayerCreateSchema(player_name='Cande')
+    player_test_2_id = client.post('/player', json=player_test_2.model_dump())
+    player_test_2_json = player_test_2_id.json()
+    assert (
+        'player_id' in player_test_2_json
+    ), "El 'player_id' no se encuentra en la respuesta del servidor"
+    player_id_2 = player_test_2_json['player_id']
+
+    lobby_test = LobbyCreateSchema(
+        lobby_name='LobbyTest',
+        lobby_owner=player_id_1,
+        min_players=0,
+        max_players=4,
+    )
+    lobby_test_id = client.post('/lobby', json=lobby_test.model_dump())
+
+    lobby_json = lobby_test_id.json()
+    assert (
+        'lobby_id' in lobby_json
+    ), "El 'lobby_id' no se encuentra en la respuesta del servidor"
+    lobby_id = lobby_json['lobby_id']
+
+    data_player_1 = {
+        'player_id': player_id_1,
+        'lobby_id': lobby_id,
+    }
+    data_player_2 = {
+        'player_id': player_id_2,
+        'lobby_id': lobby_id,
+    }
+
+    client.post('lobby/join', json=data_player_1)
+    client.post('lobby/join', json=data_player_2)
+
+    db = next(get_db())
+    delete_player(db=db, player_id=player_id_2)
+
+    game_test = GameCreate(lobby_id=lobby_id, player_id=player_id_1)
+    response = client.post('/game', json=game_test.model_dump())
+    assert (
+        response.status_code == 404
+    ), f'Se esperaba un status code 400, pero se recibió {response.status_code}'
+    assert (
+        response.json()['detail'] == errors.PLAYER_IS_MISSING
+    ), f"Mensaje de error incorrecto: {response.json()['detail']}"
+
+
+def test_leave_game():
+    player_test_1 = PlayerCreateSchema(player_name='Santino')
+    player_test_1_id = client.post('/player', json=player_test_1.model_dump())
+    player_test_1_json = player_test_1_id.json()
+    assert (
+        'player_id' in player_test_1_json
+    ), "El 'player_id' no se encuentra en la respuesta del servidor"
+    player_id_1 = player_test_1_json['player_id']
+
+    player_test_2 = PlayerCreateSchema(player_name='Cande')
+    player_test_2_id = client.post('/player', json=player_test_2.model_dump())
+    player_test_2_json = player_test_2_id.json()
+    assert (
+        'player_id' in player_test_2_json
+    ), "El 'player_id' no se encuentra en la respuesta del servidor"
+    player_id_2 = player_test_2_json['player_id']
+
+    lobby_test = LobbyCreateSchema(
+        lobby_name='LobbyTest',
+        lobby_owner=player_id_1,
+        min_players=0,
+        max_players=4,
+    )
+    lobby_test_id = client.post('/lobby', json=lobby_test.model_dump())
+
+    lobby_json = lobby_test_id.json()
+    assert (
+        'lobby_id' in lobby_json
+    ), "El 'lobby_id' no se encuentra en la respuesta del servidor"
+    lobby_id = lobby_json['lobby_id']
+
+    data_player_1 = {
+        'player_id': player_id_1,
+        'lobby_id': lobby_id,
+    }
+    data_player_2 = {
+        'player_id': player_id_2,
+        'lobby_id': lobby_id,
+    }
+
+    client.post('lobby/join', json=data_player_1)
+    client.post('lobby/join', json=data_player_2)
+
+    game_test = GameCreate(lobby_id=lobby_id, player_id=player_id_1)
+    client.post('/game', json=game_test.model_dump())
+    response = client.post(
+        '/game/leave',
+        json={'playerId': player_id_2},
+    )
+    assert response.status_code == 200
+
+
+def test_leave_game_error_game_not_found():
+    player_test_1 = PlayerCreateSchema(player_name='Santino')
+    player_test_1_id = client.post('/player', json=player_test_1.model_dump())
+    player_id_1 = player_test_1_id.json()['player_id']
+
+    player_test_2 = PlayerCreateSchema(player_name='Cande')
+    player_test_2_id = client.post('/player', json=player_test_2.model_dump())
+    player_id_2 = player_test_2_id.json()['player_id']
+
+    lobby_test = LobbyCreateSchema(
+        lobby_name='LobbyTest',
+        lobby_owner=player_id_1,
+        min_players=0,
+        max_players=4,
+    )
+    lobby_test_id = client.post('/lobby', json=lobby_test.model_dump())
+    lobby_id = lobby_test_id.json()['lobby_id']
+
+    client.post('/lobby/join', json={'player_id': player_id_1, 'lobby_id': lobby_id})
+    client.post('/lobby/join', json={'player_id': player_id_2, 'lobby_id': lobby_id})
+
+    game_test = GameCreate(lobby_id=lobby_id, player_id=player_id_1)
+    client.post('/game', json=game_test.model_dump())
+
+    db = next(get_db())
+    game = get_game(db=db, player_id=player_id_1)
+    if game:
+        delete_game(db=db, game_id=str(game.game_id))
+
+    response = client.post('/game/leave', json={'playerId': player_id_2})
+
+    assert (
+        response.status_code == 404
+    ), f'Se esperaba un status code 404, pero se recibió {response.status_code}'
+    assert (
+        response.json()['detail'] == errors.GAME_NOT_FOUND
+    ), f"Mensaje de error incorrecto: {response.json()['detail']}"
+
+
+def test_leave_game_error_player_not_found():
+    player_test_1 = PlayerCreateSchema(player_name='Santino')
+    player_test_1_id = client.post('/player', json=player_test_1.model_dump())
+    player_id_1 = player_test_1_id.json()['player_id']
+
+    player_test_2 = PlayerCreateSchema(player_name='Cande')
+    player_test_2_id = client.post('/player', json=player_test_2.model_dump())
+    player_id_2 = player_test_2_id.json()['player_id']
+
+    lobby_test = LobbyCreateSchema(
+        lobby_name='LobbyTest',
+        lobby_owner=player_id_1,
+        min_players=0,
+        max_players=4,
+    )
+    lobby_test_id = client.post('/lobby', json=lobby_test.model_dump())
+    lobby_id = lobby_test_id.json()['lobby_id']
+
+    client.post('/lobby/join', json={'player_id': player_id_1, 'lobby_id': lobby_id})
+    client.post('/lobby/join', json={'player_id': player_id_2, 'lobby_id': lobby_id})
+
+    game_test = GameCreate(lobby_id=lobby_id, player_id=player_id_1)
+    client.post('/game', json=game_test.model_dump())
+
+    db = next(get_db())
+    delete_player(db=db, player_id=player_id_1)
+
+    response = client.post('/game/leave', json={'playerId': player_id_1})
+
+    assert (
+        response.status_code == 404
+    ), f'Se esperaba un status code 404, pero se recibió {response.status_code}'
+    assert (
+        response.json()['detail'] == errors.PLAYER_NOT_FOUND
+    ), f"Mensaje de error incorrecto: {response.json()['detail']}"
 
 
 """ LOBBY TESTS """
