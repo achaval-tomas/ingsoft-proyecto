5c1dbc7c0b975bc017fe3ffce30448cc69e19276
achaval-tomas <tomiachaval2013@gmail.com>
2024-10-07 13:58:18 -0300

    SCRUM-57 refactor MovementData class

--- src/database/cards/movement_card.py
+++ src/database/cards/movement_card.py
@@ -1,7 +1,5 @@
 from enum import Enum
 
-from pydantic import BaseModel
-
 
 class MovementType(Enum):
     DIAGONAL_ADJACENT = 'diagonal-adjacent'
@@ -13,17 +11,18 @@ class MovementType(Enum):
     STRAIGHT_SPACED = 'straight-spaced'
 
 
-class MovementData(BaseModel):
-    clamps: bool
-    target: tuple[int, int]
+class MovementData:
+    def __init__(self, clamps, target):
+        self.clamps: bool = clamps
+        self.target: tuple[int, int] = target
 
 
 movement_data = {
-    MovementType.DIAGONAL_ADJACENT: MovementData(clamps=False, target=[1, 1]),
-    MovementType.DIAGONAL_SPACED: MovementData(clamps=False, target=[2, 2]),
-    MovementType.L_CCW: MovementData(clamps=False, target=[2, 1]),
-    MovementType.L_CW: MovementData(clamps=False, target=[1, 2]),
-    MovementType.STRAIGHT_ADJACENT: MovementData(clamps=False, target=[1, 0]),
-    MovementType.STRAIGHT_EDGE: MovementData(clamps=True, target=[5, 0]),
-    MovementType.STRAIGHT_SPACED: MovementData(clamps=False, target=[2, 0]),
+    MovementType.DIAGONAL_ADJACENT: MovementData(clamps=False, target=(1, 1)),
+    MovementType.DIAGONAL_SPACED: MovementData(clamps=False, target=(2, 2)),
+    MovementType.L_CCW: MovementData(clamps=False, target=(2, 1)),
+    MovementType.L_CW: MovementData(clamps=False, target=(1, 2)),
+    MovementType.STRAIGHT_ADJACENT: MovementData(clamps=False, target=(1, 0)),
+    MovementType.STRAIGHT_EDGE: MovementData(clamps=True, target=(5, 0)),
+    MovementType.STRAIGHT_SPACED: MovementData(clamps=False, target=(2, 0)),
 }
