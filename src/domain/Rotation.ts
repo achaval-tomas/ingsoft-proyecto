e733447d59f56b1e5b562754b43babb593502aaa
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-10-08 15:51:23 -0300

    SCRUM-98 add Rotation schema

--- src/domain/Rotation.ts
+++ src/domain/Rotation.ts
@@ -1,1 +1,4 @@
-export type Rotation = "r0" | "r90" | "r180" | "r270";
+import { z } from "zod";
+
+export const RotationSchema = z.enum(["r0", "r90", "r180", "r270"]);
+export type Rotation = z.infer<typeof RotationSchema>;
