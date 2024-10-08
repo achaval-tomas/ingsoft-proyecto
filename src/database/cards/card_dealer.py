af9393a1926e0f18de9f84ce73c1254f1bbeb230
achaval-tomas <tomiachaval2013@gmail.com>
2024-10-08 14:50:05 -0300

    SCRUM-92 Hand blue and white cards equally for each player

--- src/database/cards/card_dealer.py
+++ src/database/cards/card_dealer.py
@@ -1,26 +1,47 @@
 from random import shuffle
 
 from src.database.cards.movement_card import movement_data
-from src.database.cards.shape_card import ShapeType
+from src.database.cards.shape_card import shape_data
 from src.schemas.card_schemas import ShapeCardSchema
 
 
 class ShapeCardDealer:
     def __init__(self, nplayers: int):
-        shape_cards = [
+        shape_cards_b = [
             ShapeCardSchema(
-                shape=c.value,
+                shape=c,
                 isBlocked=False,
             )
-            for c in ShapeType
+            for c in shape_data
+            if c[0] == 'b'
         ] * 2
-        shuffle(shape_cards)
-        self.deck = shape_cards
-        self.cards_per_player = len(shape_cards) // nplayers
+        shape_cards_c = [
+            ShapeCardSchema(
+                shape=c,
+                isBlocked=False,
+            )
+            for c in shape_data
+            if c[0] == 'c'
+        ] * 2
+
+        shuffle(shape_cards_b)
+        shuffle(shape_cards_c)
+
+        self.deck_b = shape_cards_b
+        self.deck_c = shape_cards_c
+
+        self.b_cards_pp = len(shape_cards_b) // nplayers
+        self.c_cards_pp = len(shape_cards_c) // nplayers
 
     def deal_shape_cards(self):
-        player_cards = self.deck[0 : self.cards_per_player]
-        self.deck = self.deck[self.cards_per_player :]
+        player_cards = self.deck_b[0 : self.b_cards_pp]
+        player_cards += self.deck_c[0 : self.c_cards_pp]
+
+        shuffle(player_cards)
+
+        self.deck_c = self.deck_c[self.c_cards_pp :]
+        self.deck_b = self.deck_b[self.b_cards_pp :]
+
         return player_cards
 
 
