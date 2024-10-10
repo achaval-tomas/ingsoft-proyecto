eae188bd9b7f5fcc441d06e402fc340e8d255a6d
Tomás Peyronel <tpeyronel@gmail.com>
2024-10-10 19:55:55 -0300

    SCRUM-116 improve selectable tiles pulse color

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,9 +27,9 @@ export default {
                     "100%": { backgroundColor: "rgba(255, 255, 255, 0.3)" },
                 },
                 selectable: {
-                    "0%": {  border: "2.5px solid rgb(255, 255, 255)" },
-                    "50%": {  border: "2.5px solid rgb(160, 160, 160)" },
-                    "100%": {  border: "2.5px solid rgb(255, 255, 255)" },
+                    "0%": {  border: "2.5px solid rgba(255, 255, 255, 1.0)" },
+                    "50%": {  border: "2.5px solid rgba(255, 255, 255, 0.4)" },
+                    "100%": {  border: "2.5px solid rgba(255, 255, 255, 1.0)" },
                 },
             },
         },
