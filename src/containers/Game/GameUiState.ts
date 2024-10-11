6ee4d8fe3696193fe28bb01b0d541d4c87b5b970
Tomás Peyronel <tpeyronel@gmail.com>
2024-10-11 12:27:26 -0300

    SCRUM-116 define GameUiState and all the other -UiState types

---
+++ src/containers/Game/GameUiState.ts
@@ -0,0 +1,52 @@
+import { Color } from "../../domain/Color";
+import { Direction } from "../../domain/Direction";
+import { CommonPlayerState } from "../../domain/GameState";
+import { Movement } from "../../domain/Movement";
+import { Shape } from "../../domain/Shape";
+
+export type BoardTileStatus = "normal" | "highlighted" | "selected" | "selectable";
+
+export type BoardTileUiState = {
+    color: Color;
+    status: BoardTileStatus;
+}
+
+export type BoardUiState = {
+    tiles: BoardTileUiState[];
+    activeSide: Direction;
+}
+
+export type ShapeCardStatus = "normal" | "blocked" | "selected";
+
+export type ShapeCardUiState = {
+    shape: Shape;
+    status: ShapeCardStatus;
+}
+
+export type CommonPlayerUiState = {
+    name: string;
+    shapeCardsInHand: ShapeCardUiState[];
+    shapeCardsInDeckCount: number;
+}
+
+export type MovementCardStatus = "normal" | "selected";
+
+export type MovementCardUiState = {
+    movement: Movement;
+    status: MovementCardStatus;
+}
+
+export type SelfPlayerUiState = CommonPlayerUiState & {
+    movementCardsInHand: MovementCardUiState[];
+}
+
+export type OtherPlayerUiState = CommonPlayerUiState & {
+    movementCardsInHandCount: number;
+}
+
+export type GameUiState = {
+    selfPlayerUiState: SelfPlayerUiState;
+    otherPlayersUiState: OtherPlayerUiState[];
+    boardUiState: BoardUiState;
+    winner: CommonPlayerState | undefined;
+}
