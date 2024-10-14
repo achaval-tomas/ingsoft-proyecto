ff829303b0d7d5dfb77efe00b5e19d5681f4e132
Tomás Peyronel <tpeyronel@gmail.com>
2024-10-13 23:25:40 -0300

    SCRUM-122 add optional error field to Field

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -6,6 +6,7 @@ interface FieldProps {
     onChange : (value: string) => void;
     label: string;
     placeholder?: string;
+    error?: string;
     type?: HTMLInputTypeAttribute;
     min?: number | string;
     max?: number | string;
@@ -13,7 +14,7 @@ interface FieldProps {
     disabled?: boolean;
 }
 
-function Field ({ value, onChange, label, placeholder, type, min, max, inputTestId, disabled = false }: FieldProps) {
+function Field ({ value, onChange, label, placeholder, error, type, min, max, inputTestId, disabled = false }: FieldProps) {
     return <HUiField>
         <Label>{label}</Label>
         <Input
@@ -27,6 +28,7 @@ function Field ({ value, onChange, label, placeholder, type, min, max, inputTest
             max={max}
             data-testid={inputTestId}
         />
+        {(error != null) && <p className="mt-2.5 text-sm !text-red-400">{error}</p>}
     </HUiField>;
 }
 
