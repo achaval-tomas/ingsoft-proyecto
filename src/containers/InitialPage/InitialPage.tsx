65ae82171c2d57862d2cb66ab135194b63e89660
Tomás Peyronel <tpeyronel@gmail.com>
2024-10-13 21:57:01 -0300

    SCRUM-122 implement fancy title

--- src/containers/InitialPage/InitialPage.tsx
+++ src/containers/InitialPage/InitialPage.tsx
@@ -18,14 +18,26 @@ function InitialPage() {
         }
     }
 
-    return <div className="flex flex-col align-center w-screen px-16">
-        <h1 className="my-16 text-center">El Switcher</h1>
-        <PlayerNameForm handleSubmit={name => void createPlayerHandler(name)} />
-        { serverError &&
-            <p className="text-center text-red-500 py-2">
-                Hay un error en el servidor, por favor intentá más tarde
-            </p>
-        }
+    return <div className="w-screen h-screen grid items-center gap-4" style={{ gridTemplateRows: "1fr 1fr 1fr" }}>
+        <div className="w-full drop-shadow-[0_0_8px_#f6931e]">
+            <div className="w-full bg-[#f6931e] py-1">
+                <div className="w-full bg-black py-1">
+                    <div className="w-full bg-[#f6931e] py-1">
+                        <div className="w-full bg-black py-1">
+                            <h1 className="w-full text-center bg-[#f6931e] p-4 text-black text-[5rem]" style={{ fontFamily: "seaside-resort" }}>El Switcher</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="w-full flex-grow flex justify-center items-center">
+            <PlayerNameForm handleSubmit={name => void createPlayerHandler(name)} />
+            { serverError &&
+                <p className="text-center text-red-500 py-2">
+                    Hay un error en el servidor, por favor intentá más tarde
+                </p>
+            }
+        </div>
     </div>;
 }
 
