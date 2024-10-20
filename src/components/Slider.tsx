f65d3d506335bb953959b52695208dc439c788bb
Santino Andres Quevedo Rodriguez <santino.quevedo@mi.unc.edu.ar>
2024-10-20 09:18:41 -0300

    SCRUM-117-refactoring-filters

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,48 +1,44 @@
-import { Switch } from "@headlessui/react";
 import React from "react";
 
 
 export type SliderProps = {
     playerCountValue: number;
     onPlayerCountChange: (value: number) => void;
-    searchByPCState: boolean;
     onSearchByPCChange: (switchState: boolean) => void;
 };
 
 
 const Slider = React.forwardRef<HTMLDivElement, SliderProps>(
-    function Slider({ playerCountValue, onPlayerCountChange, searchByPCState, onSearchByPCChange }, ref) {
+    function Slider({ playerCountValue, onPlayerCountChange, onSearchByPCChange }, ref) {
 
-    return <div 
-                className="self-end mr-4"
-                ref={ref}>
-                <Switch
-                    checked={searchByPCState}
-                    onChange={onSearchByPCChange}
-                    className="group flex h-6 w-12 cursor-pointer rounded-full bg-zinc-900/50 transition p-1 duration-200 data-[checked]:bg-primary-600/50 m-2"
-                >
-                    <span
-                        aria-hidden="true"
-                        className="relative size-4 rounded-full ring-4 ring-primary-600 bg-white transition duration-200 group-data-[checked]:translate-x-6"
-                    />
-                </Switch>
-                <input className="mb-0 mx-2 w-full cursor-pointer" 
+        const handleSliderChange = (value: number) => {
+            onSearchByPCChange(true);
+            onPlayerCountChange(value);
+        };
+
+        return (
+            <div className="self-end mr-4" ref={ref}>
+                <input
+                    className="mb-0 mx-2 w-full cursor-pointer"
                     type="range"
-                    disabled={!searchByPCState}
                     value={playerCountValue}
-                    onChange={e => onPlayerCountChange(parseInt(e.target.value))}
+                    onChange={e => handleSliderChange(parseInt(e.target.value))}
                     onClick={e => e.preventDefault()}
-                    min={1}
+                    min={0}
                     max={3}
-                    />
+                />
                 <div className="flex justify-between mx-2 mt-0 mb-2 w-full">
-                    {[1, 2, 3].map((amount) => (
-                        <span 
+                    {[0, 1, 2, 3].map((amount) => (
+                        <span
                             className="mx-1.5 font-bold"
-                            key={amount}>{amount}</span>          
+                            key={amount}>{amount}</span>
                     ))}
                 </div>
             </div>
-});
+        );
+    }
+);
 
 export default Slider
+
+
--- src/containers/MainPage/components/LobbysFilter.tsx
+++ src/containers/MainPage/components/LobbysFilter.tsx
@@ -1,68 +1,56 @@
-import { Menu, MenuButton, MenuItem, MenuItems, MenuSeparator, Switch } from "@headlessui/react"
+import { Menu, MenuButton, MenuItem, MenuItems, MenuSeparator } from "@headlessui/react"
 import Slider, { SliderProps } from "../../../components/Slider"
 import FilledButton from "../../../components/FilledButton";
 
 type FilterProps = SliderProps & {
     lobbyNameValue: string;
     onLobbyNameChange: (value: string) => void;
-    searchByNameState: boolean;
-    onChangeSearchByNameState: () => void;
     onResetQuerys: () => void;
 }
 
-function LobbysFilter ({ 
-    playerCountValue, 
-    onPlayerCountChange, 
-    searchByPCState, 
+function LobbysFilter({
+    playerCountValue,
+    onPlayerCountChange,
     onSearchByPCChange,
     lobbyNameValue,
     onLobbyNameChange,
-    searchByNameState,
-    onChangeSearchByNameState,
     onResetQuerys }: FilterProps) {
-
-    return  <div 
-                className="self-end">
+    return (
+        <div className="self-end">
             <Menu>
                 <MenuButton className="rounded-lg bg-primary-600 hover:bg-primary-500 data-[active]:bg-primary-500 transition-colors px-4 py-2 mx-2 mb-3">
                     Filtros
-                    <i className="fa-solid fa-chevron-down ml-2"/>
+                    <i className="fa-solid fa-chevron-down ml-2" />
                 </MenuButton>
                 <MenuItems
                     anchor="bottom end"
                     className="bg-zinc-700/90 w-80 [--anchor-gap:2px] rounded-lg">
-                    <MenuItem
-                        as="div">
-                        <Switch
-                        checked={searchByNameState}
-                        onChange={onChangeSearchByNameState}
-                        className="group flex h-6 w-12 cursor-pointer rounded-full bg-zinc-900/50 transition p-1 duration-200 data-[checked]:bg-primary-600/50 m-2">
-                            <span
-                                aria-hidden="true"
-                                className="relative size-4 rounded-full ring-4 ring-primary-600 bg-white transition duration-200 group-data-[checked]:translate-x-6"
-                            />
-                        </Switch>
+
+
+                    <MenuItem as="div">
                         <input
                             type="string"
-                            disabled={!searchByNameState}
                             value={lobbyNameValue}
                             onChange={e => onLobbyNameChange(e.target.value)}
                             onClick={e => e.preventDefault()}
                             placeholder="Nombre de la sala"
-                            className="px-3 py-1.5 rounded-lg bg-black/50 ml-2 text-sm/6 text-white disabled:bg-black/20"
-                            />
+                            className="px-3 py-1.5 rounded-lg bg-black/50 ml-2 text-sm/6 text-white"
+                        />
                     </MenuItem>
+
                     <MenuSeparator className="m-3 h-0.5 rounded-lg bg-white/80" />
-                    <MenuItem
-                        as="div">
+
+
+                    <MenuItem as="div">
                         <Slider
                             playerCountValue={playerCountValue}
                             onPlayerCountChange={onPlayerCountChange}
-                            searchByPCState={searchByPCState}
                             onSearchByPCChange={onSearchByPCChange}
                         />
                     </MenuItem>
+
                     <MenuSeparator className="m-3 h-0.5 rounded-lg bg-white/80" />
+
                     <MenuItem
                         as="div"
                         className="m-2"
@@ -71,7 +59,9 @@ function LobbysFilter ({
                     </MenuItem>
                 </MenuItems>
             </Menu>
-            </div>
+        </div>
+    );
 };
 
 export default LobbysFilter
+
