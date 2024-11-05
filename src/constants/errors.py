e4778f5a17e28cf390fe5b5ecf847adef00a9d0c
achaval-tomas <tomiachaval2013@gmail.com>
2024-11-05 09:00:13 -0300

    SCRUM-137 add error message for blockcard fail

--- src/constants/errors.py
+++ src/constants/errors.py
@@ -22,6 +22,7 @@ NOT_ENOUGH_PLAYERS = (
 PLAYER_IS_MISSING = 'No se pudo encontrar a uno de los jugadores'
 
 INVALID_CARD = 'No puedes utilizar esta carta!'
+ALREADY_BLOCKED = 'El jugador ya tiene una carta bloqueada!'
 
 MOVEMENT_OUT_OF_BOUNDS = 'No es posible realizar este movimiento'
 NO_MOVEMENTS_TO_CANCEL = 'No tienes movimientos para cancelar!'
