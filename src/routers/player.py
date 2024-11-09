872ab331cafe25066a77318906e64e0b162d5bd2
achaval-tomas <tomiachaval2013@gmail.com>
2024-11-09 16:37:14 -0300

    SCRUM-159 post /player now creates a user

--- src/routers/player.py
+++ src/routers/player.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
 from src.constants import errors
-from src.database.crud import crud_player
+from src.database.crud import crud_user
 from src.database.db import SessionDep
 from src.schemas.player_schemas import PlayerCreateSchema, PlayerIdSchema
 
@@ -13,10 +13,10 @@ create_player_router = APIRouter()
     status_code=201,
     response_model=PlayerIdSchema,
 )
-async def create_player(player: PlayerCreateSchema, db: SessionDep):
-    if not player.player_name:
+async def create_player(user_info: PlayerCreateSchema, db: SessionDep):
+    if not user_info.player_name:
         raise HTTPException(status_code=400, detail=errors.EMPTY_NAME)
 
     return PlayerIdSchema(
-        player_id=crud_player.create_player(db=db, player=player),
+        player_id=crud_user.create_user(db=db, user=user_info),
     )
