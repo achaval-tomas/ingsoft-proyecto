6ac15dcbfe21dd3826e543ac3199c04764992959
Tomás Peyronel <tpeyronel@gmail.com>
2024-11-09 12:49:23 -0300

    SCRUM-147 restore chat scrolling

--- src/containers/Game/components/Chat.tsx
+++ src/containers/Game/components/Chat.tsx
@@ -23,14 +23,16 @@ const Chat = ({ messages, onSendMessage }: ChatProps) => {
     return (
         <div className="h-full flex flex-col justify-end">
             <div
-                className="h-full mb-2 flex flex-col justify-end overflow-y-auto break-words"
+                className="h-full mb-2 justify-end overflow-y-auto"
                 style={{ "WebkitMaskImage": "linear-gradient(0deg, #000 80%, transparent)" }}
             >
-                {messages.map((message, i) => (
-                    <div key={i}>
-                        <strong>{message.sender}:</strong> {message.text}
-                    </div>
-                ))}
+                <div className="flex flex-col break-words">
+                    {messages.map((message, i) => (
+                        <div key={i}>
+                            <strong>{message.sender}:</strong> {message.text}
+                        </div>
+                    ))}
+                </div>
             </div>
             <form onSubmit={(e) => { e.preventDefault(); sendMessage(); }}>
                 <Input
