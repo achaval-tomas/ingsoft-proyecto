e5585009925ba2a0e0915cbb0bfaf84a9fc8f54a
Tomás Peyronel <tpeyronel@gmail.com>
2024-11-08 21:48:02 -0300

    SCRUM-151 define Notification

--- src/domain/AppState.ts
+++ src/domain/AppState.ts
@@ -1,7 +1,9 @@
 import { GameState } from "./GameState";
+import Notification from "./Notification";
 
 type AppState = {
     gameState: GameState | null;
+    notifications: Notification[];
 }
 
 export default AppState;
