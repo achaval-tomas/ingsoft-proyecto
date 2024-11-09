a0dafc51f2d501c0186bac5ff539e87243c70034
candechapuis <candelariachapuis@gmail.com>
2024-11-09 19:11:49 -0300

    SCRUM-140: minimal correction

--- src/tools/hashingfy.py
+++ src/tools/hashingfy.py
@@ -5,5 +5,5 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(pw: str):
     return pwd_context.hash(pw)
 
-def veriy_password(plain_pw: str, hashed_pw: str):
+def verify_password(plain_pw: str, hashed_pw: str):
     return pwd_context.verify(plain_pw, hashed_pw)
