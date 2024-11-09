cf7b145252f85b3cac2b8855d165336ee86fde57
Tomás Peyronel <tpeyronel@gmail.com>
2024-11-09 11:23:07 -0300

    SCRUM-153 add sender to ChatMessage

--- src/domain/ChatMessage.ts
+++ src/domain/ChatMessage.ts
@@ -5,6 +5,9 @@ export type ChatMessageType = z.infer<typeof ChatMessageTypeSchema>;
 
 export const ChatMessageSchema = z.object({
     type: ChatMessageTypeSchema,
+    // The name of the player that "sent" the message, or undefined.
+    // It is separate from 'text' for styling purposes.
+    sender: z.string().optional(),
     text: z.string(),
 });
 
