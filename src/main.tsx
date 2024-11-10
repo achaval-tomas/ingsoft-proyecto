522f5db47ab70f9e63fef0a870f213c113a37465
Tomás Peyronel <tpeyronel@gmail.com>
2024-11-10 13:26:52 -0300

    SCRUM-167 add gameId to /play path

--- src/main.tsx
+++ src/main.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
                 element: <Lobby />,
             },
             {
-                path: "play",
+                path: "play/:gameId",
                 element: <GamePage />,
             },
         ],
