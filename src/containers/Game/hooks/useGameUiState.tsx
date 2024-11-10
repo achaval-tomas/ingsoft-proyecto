b02417a58e624ee3cda7a245cee67fcaec4548af
Tomás Peyronel <tpeyronel@gmail.com>
2024-11-09 21:08:51 -0300

    SCRUM-136 fix shape of someone's blocked shape being highlighted if it is their only shape card remaining

--- src/containers/Game/hooks/useGameUiState.tsx
+++ src/containers/Game/hooks/useGameUiState.tsx
@@ -38,10 +38,11 @@ function useGameUiState(
     const shapeWhitelist = useMemo(
         () => selfPlayerState.shapeCardsInHand
             .filter(sc => !sc.isBlocked || selfPlayerState.shapeCardsInHand.length === 1)
-            .concat(otherPlayersState
-                .map(p => p.shapeCardsInHand
-                    .filter(sc => !sc.isBlocked || p.shapeCardsInHand.length === 1),
-                ).flat())
+            .concat(
+                otherPlayersState
+                    .map(p => p.shapeCardsInHand.filter(sc => !sc.isBlocked))
+                    .flat(),
+            )
             .map(s => s.shape),
         [selfPlayerState, otherPlayersState],
     );
