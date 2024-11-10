a241e05553b770cc8f5e9ead45c121264aac8f04
Tomás Peyronel <tpeyronel@gmail.com>
2024-11-10 12:17:06 -0300

    SCRUM-172 exclude all config files from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setupTests.ts",
-  },
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./src/setupTests.ts",
+        coverage: {
+            exclude: [...coverageConfigDefaults.exclude, "*.config.*"],
+        },
+    },
 });
-
