ca030dd9a6ea0c8db23eeefe7c4f17ef1b0e6769
candechapuis <candelariachapuis@gmail.com>
2024-11-10 03:06:13 -0300

    SCRUM-141: lint and refactor token functions

--- src/tools/tokenify.py
+++ src/tools/tokenify.py
@@ -1,13 +1,15 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
+
+import jwt
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-import jwt
 
 from src.database.crud.crud_player import get_player
 
 SECRET_KEY = "0f1e038500d9926dc570a385b1bec35af3fdbf11d72056d74028dfaa38f4d120"
-ALGORITHM = "HS256" 
+ALGORITHM = "HS256"
+EXCEPTION = HTTPException(status_code=401, detail="Couldn't validate token")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="lobby/join")
 
@@ -18,16 +20,17 @@ def create_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
     return encoded_jwt
 
-def validate_token(token: Annotated[str, Depends(oauth2_scheme)]): # when game starts, when entering an active game?
+def validate_token(token: Annotated[str, Depends(oauth2_scheme)]): 
+    # when game starts, when entering an active game?
     try:
         payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
         player_id = payload.get("sub")
         if not player_id:
-            raise HTTPException(status_code=401, detail="Couldn't validate token")
+            raise EXCEPTION
     except jwt.InvalidTokenError:
-        raise HTTPException(status_code=401, detail="Couldn't validate token")
+        raise EXCEPTION
     # all tokens (for now) will be created w/ player id as the payload.
     player = get_player(player_id)
     if player is None:
-        raise HTTPException(status_code=401, detail="Couldn't validate token")
+        raise EXCEPTION
     return player
