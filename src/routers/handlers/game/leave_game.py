4a52f523edb776c5ae111ec1222916aa59a139ba
achaval-tomas <tomiachaval2013@gmail.com>
2024-11-10 15:56:42 -0300

    SCRUM-174 Fix leave_game endpoint

--- src/routers/handlers/game/leave_game.py
+++ src/routers/handlers/game/leave_game.py
@@ -1,34 +1,36 @@
 from sqlalchemy.orm import Session
 
 from src.database.crud import crud_game
-from src.database.crud.crud_user import (
-    decode_player_id,
-    delete_active_player,
-)
+from src.database.crud.crud_user import delete_active_player, get_player
 from src.routers.handlers.game.announce_winner import handle_announce_winner
 from src.routers.helpers.connection_manager import game_manager
 from src.schemas.player_schemas import PlayerMessageSchema
+from src.tools.jsonify import deserialize
 
 
 async def handle_leave_game(user_id: str, player_id: str, game_id: str, db: Session):
     res, winner_id = crud_game.leave_game(db=db, player_id=player_id)
+
+    if res != 0 or res != 3:
+        return res
+
+    game = crud_game.get_game(db, game_id)
+    if not game:
+        return 1
+
+    for id in deserialize(game.player_order):
+        player = get_player(db, id)
+        if player:
+            await game_manager.send_personal_message(
+                player_id=id,
+                message=PlayerMessageSchema(
+                    type='player-left',
+                    playerId=user_id if id == player_id else player_id,
+                ).model_dump_json(),
+            )
     delete_active_player(db, player_id, player_id)
 
-    if res == 0 or res == 3:
-        await game_manager.broadcast_in_game(
-            db=db,
-            game_id=game_id,
-            message=PlayerMessageSchema(
-                type='player-left',
-                playerId=decode_player_id(
-                    db=db,
-                    user_id=user_id,
-                    player_id=player_id,
-                ),
-            ).model_dump_json(),
-        )
-        if res == 3:
-            await handle_announce_winner(db=db, winner_id=winner_id)
-        return 0
+    if res == 3:
+        await handle_announce_winner(db=db, winner_id=winner_id)
 
-    return res
+    return 0
